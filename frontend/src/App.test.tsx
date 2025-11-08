fc9527e19d02da199bbb8b3fab15d109da26e95c
Brett Pyke <brett@brettpyke.com>
2025-11-08 15:01:03 +1100

    Initial Frontend Commit

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,10 @@
+import { expect, describe, it } from "vitest";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  it("should render correctly on the first try", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Vite + React")).toBeDefined();
+  });
+});
